cf607ba3292015d537b5cfdb03f6adfdee407f5e
drand808 <drand808@live.kutztown.edu>
2024-02-22 13:36:18 -0500

    Initial functionality for project2

---
+++ sprint2/project2School/program2dnr.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class program2dnr {
+  // numbers can be >= 0, like paying off your car payment
+  // they can change the savings percentage
+  // make functions public
+  // that's a bold statement, why do you think that
+  // split checking double value into two functions, and check both in while loop
+  //   - validateDouble, validateDoubleValue. Check for oduble input first, then number
+  
+  public static void main(String[] args) { 
+    // Create a Scanner
+    Scanner input = new Scanner(System.in);
+    
+    // Tell user the usage of program and that they are entering the first month
+    double[][] yearData = new double[8][12];
+    
+    // Get initial data
+    // {salary, rent, car, gas, food, savings, fun, total}
+    double[] monthData = new double[8];
+    monthData = getFirstMonth(input);
+    
+    // Get savings percent
+    double savingsPercent = getInputDouble(input, "Enter savings percentage (ex: 20.5): ");
+    
+    // Get savings and fun based off salary and savingsPercent
+    // Restructure to update the monthData directly? monthData = updateForExtraMoney
+    double[] extraMoney = getDisposableIncome(monthData, savingsPercent);
+    System.out.println("savings: " + extraMoney[0] + "\nFun: " + extraMoney[1] + "\nTotal: " + extraMoney[2]);
+    monthData[5] = extraMoney[0];
+    monthData[6] = extraMoney[1];
+    monthData[7] = extraMoney[2];
+    
+    // Loop through the rest of the months
+    for(int month = 1; month < 12; month++){
+      continue;
+    }
+  }
+  
+  private static double[] getDisposableIncome(double[] monthData, double savingsPercent){
+    double expenses = 0.0;
+    for(int category = 1; category < 5; category++){
+      expenses += monthData[category];
+    }
+    double disposableIncome = monthData[0] - expenses;
+    double savings = disposableIncome * (savingsPercent/100);
+    double fun = disposableIncome - savings;
+    double total = expenses + savings + fun;
+    return new double[] {savings, fun, total};
+  }
+  
+  private static double[] getFirstMonth(Scanner input){
+    double salary = getInputDouble(input, "Enter monthly salary: ");
+    double rent = getInputDouble(input, "Enter monthly rent: ");
+    double car = getInputDouble(input, "Enter monthly car: ");
+    double gas = getInputDouble(input, "Enter monthly gas: ");
+    double food = getInputDouble(input, "Enter monthly food: ");
+    return new double[] {salary, rent, car, gas, food, 0, 0, 0};
+  }
+  
+  private static double getInputDouble(Scanner input, String prompt){
+    double num = -1.0;
+    // Loop until valid input
+    do {
+      System.out.print(prompt);
+      num = getNextInputDouble(input);
+    }
+    while (!validateInputDouble(num, 0.0));
+    return num;
+  }
+
+  private static double getNextInputDouble(Scanner input){
+    // Test if input is an integer
+    try {
+      return input.nextDouble();
+    }
+    catch (Exception e){
+      System.out.println(" > ERROR: Please make sure you are entering a number");
+      input.nextLine(); // Removes input from the line, assume one-line input
+      return -1;
+    }
+  }
+
+  private static boolean validateInputDouble(double inputNum1, double constraint){
+    if (inputNum1 > constraint){
+      return true;
+    }
+    System.out.println(" > ERROR: Please enter a number greater than " + constraint);
+    return false;
+  }
+}
