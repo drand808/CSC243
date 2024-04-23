a27b417814cbf3f7e00bd93b8d245a35eeca4602
drand808 <drand808@live.kutztown.edu>
2024-04-23 14:32:10 -0400

    created project. uses fxml for GUI

---
+++ sprint6/project/src/mainstageController.java
@@ -0,0 +1,14 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class mainstageController {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private void handleButtonClick() {
+        System.out.println("Button clicked!");
+        // Add your event handling logic here
+    }
+}
